cff97f7d444f9881e5d60866dbedbcbfb29aa843
masengeshocadeau <masengeshocadeau603@gmail.com>
2024-02-05 13:42:54 +0200

    connect frontend and backend

--- Frontend/src/utils/config.js
+++ Frontend/src/utils/config.js
@@ -1,3 +1,3 @@
- export const BASE_URL = 'http://localhost:4000/api/v1';
+ export const BASE_URL = 'https://tour-backend-0q3c.onrender.com/api/v1';
 
 // export const BASE_URL = 'https://tour-backend-9z2z.onrender.com'
